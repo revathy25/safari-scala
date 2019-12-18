82a3ea8b8f4f0375411cb3cc1336a1abe47a2b11
Simon Roberts <simon@dancingcloudservices.com>
2019-12-18 09:09:53 -0700

    Break One

--- src/main/scala/hello/Hello.scala
+++ src/main/scala/hello/Hello.scala
@@ -1,7 +1,23 @@
 package hello
 
-object Hello {
+object Hello { // singleton...
   def main(args: Array[String]): Unit = {
-    Console println "Hello Scala World!"
+    println("Hello Scala World!")
+
+    val x:Int = 99 // explicit type
+    val y = 99 // inferred type
+//    x = 100 // val cannot be reassigned (c.f. final, const in Java, C#, JS)
+
+    var count = 99 ; count += 1; count = count + 1
+//    count++ // Scala does not have ++ --
+    println("count is now " + count)
+    // Scala is strongly STATICALLY typed
+//    count = 99.9
+
+    // mostly the usual suspects for arithmetic and comparisons
+    // + - * / < <= > >= !=
+    // what about == ... yes usually appropriate
+    // == defers to equals() method
+    
   }
 }
---
+++ src/main/scala/newpackage/NewThing.scala
@@ -0,0 +1,5 @@
+package newpackage
+
+object NewThing {
+
+}
